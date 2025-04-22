9c4fc763ecf33b1d76048a9f5c486c1a5006daf4
NesanThivyan <thivyanesan25@gmail.com>
2025-04-22 16:13:23 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer(){
+    return(
+        <>
+    <footer>
+        <div className="container">
+          <div className="col-lg-12">
+            <p>Copyright © 2036 <a href="#">Tale SEO Agency</a>. All rights reserved. 
+            
+            Design: <a href="https://templatemo.com" target="_blank" rel="noopener noreferrer">TemplateMo</a></p>
+          </div>
+        </div>
+    </footer>
+        </>
+    )
+}
+export default  Footer
